6bf395c7b816ed95a802422b24190282b29ba272
Patricia Bird <patriciabird@Patricias-MacBook-Air.local>
2021-11-10 14:26:44 -0500

    operation array obj + evaluate operation + result on screen

--- calculator.js
+++ calculator.js
@@ -1,5 +1,9 @@
 var screenText = document.getElementById("screen-text");
 var numberButtons = document.getElementsByClassName("number");
+var clearButton = document.getElementById("clear");
+var operationsButtons = document.getElementsByClassName("operation");
+
+var operationsArr = [];
 
 function stringNumToInt(stringNum) {
     if (stringNum == "zero") return 0;
@@ -14,10 +18,52 @@ function stringNumToInt(stringNum) {
     if (stringNum == "nine") return 9;
 }
 
+function operationMath(result, current){
+    if (result.operation == "plus") {
+        result.num = result.num + current.num;
+    }
+    if (result.operation == "minus") {
+        result.num = result.num - current.num;
+    }
+    if (result.operation == "times") {
+        result.num = result.num * current.num;
+    }
+    if (result.operation == "divide") {
+        result.num = result.num / current.num;
+    }
+    return result;
+}
+
 Array.from(numberButtons).forEach(numberButton => {
     numberButton.addEventListener("click", function () {
         console.log(numberButton.innerText)
         screenText.innerText += stringNumToInt(numberButton.id);
+
+    })
+})
+
+clearButton.addEventListener("click", function () {
+    screenText.innerText = "";
+})
+
+Array.from(operationsButtons).forEach(operationsButton => {
+    operationsButton.addEventListener("click", function () {
+        console.log(operationsArr.length);
+        operationsArr.push({
+            num: parseInt(screenText.innerText),
+            operation: operationsButton.id
+        });
+        console.log(JSON.parse(JSON.stringify(operationsArr)));
+        if(operationsArr.length == 2){
+            screenText.innerText = "";
+            operationsArr[0] = operationMath(operationsArr[0], operationsArr[1])
+            operationsArr[0].operation = operationsArr[1].operation;
+            operationsArr.pop();
+            screenText.innerText = operationsArr[0].num;
+        }
+        else {
+            screenText.innerText = "";
+        }
         
     })
 })
